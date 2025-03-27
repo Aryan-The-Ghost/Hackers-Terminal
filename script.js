64f67e5f3c14cd13ab84da3112e2af042d1f7e40
Aryan Mishra <155035502+Aryan-The-Ghost@users.noreply.github.com>
2025-03-27 11:38:26 +0530

    Improved Code

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-const messages = [
-    "Initializing Hacking...",
-    "Reading your Files...",
-    "Password files Detected...",
-    "Sending all passwords and personal files to server...",
-    "Cleaning up..."
-  ];
-  
-  async function hacking() {
-    const container = document.querySelector(".container");
-  
-    for (let msg of messages) {
-      const time = Math.floor(Math.random() * 7) + 1; 
-  
-      const div = document.createElement("div");
-      div.innerHTML = msg;
-      container.append(div);
-  
-      console.log(`Displayed: ${msg}`);  
-      
-      await new Promise(resolve => setTimeout(resolve, time * 1000));
-    }
+const addItem = async (item) => {
+  await randomDelay();
+  let div = document.createElement("div");
+  div.innerHTML = item;
+  document.body.append(div)
+}
+
+const randomDelay = () => {
+  return new Promise((resolve, reject) => {
+      timeout = 1 + 6 * Math.random();
+      setTimeout(() => {
+          resolve()
+      }, timeout * 1000);
+  })
+}
+
+async function main() {
+
+
+  let t = setInterval(() => {
+      let last = document.body.getElementsByTagName("div");
+      last = last[last.length - 1]
+      if(last.innerHTML.endsWith("...")){
+          last.innerHTML = last.innerHTML.slice(0, last.innerHTML.length-3)
+      }
+      else{
+
+          last.innerHTML = last.innerHTML + "."
+      }
+
+  }, 100);
+
+
+  let text = ["Initializing Hacking",
+      "Reading your Files",
+      "Password files Detected",
+      "Sending all passwords and personal files to server",
+      "Cleaning up"]
+
+  for (const item of text) {
+      await addItem(item)
   }
+
+  await randomDelay()
+  clearInterval(t)
   
-  hacking();
+}
+main()
